5fb0586113c90ce21700c9c96314da3ba73acab7
Dmitry <scannorone@gmail.com>
2017-03-03 15:50:34 +0700

    Предобработка данных в Pandas

--- w1_1_pandas_preprocessing.py
+++ w1_1_pandas_preprocessing.py
@@ -1,1 +1,24 @@
-print('Hello')
+import pandas
+
+data = pandas.read_csv('samples/titanic.csv', index_col='PassengerId')
+
+sex_count = data['Sex'].value_counts()
+male_count = sex_count['male']
+female_count = sex_count['female']
+print(male_count, female_count)
+
+passengers_number = data.count()['Name']
+print(round(data['Survived'].value_counts()[1] / passengers_number * 100, 2))
+
+print(round(data['Pclass'].value_counts()[1] / passengers_number * 100, 2))
+
+print(data['Age'].mean(), data['Age'].median())
+
+print(data.corr()['SibSp']['Parch'])
+
+females = data.where(cond=lambda d: d['Sex'] == 'female', other='')
+females_full_name = filter(lambda name: name != '', females['Name'])
+females_first_name = map(lambda name: [name.split('.')[1].split()[0].replace('(', '').replace(')', '')], females_full_name)
+females_name_data = pandas.DataFrame.from_records(data=females_first_name)
+
+print(females_name_data[0].value_counts())
