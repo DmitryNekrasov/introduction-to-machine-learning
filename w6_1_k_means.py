930213ce7d1368f1eaef10d3d216f0b8f5bab01d
Dmitry <scannorone@gmail.com>
2017-04-05 12:28:09 +0700

    K-means. Метрика PSNR.

--- w6_1_k_means.py
+++ w6_1_k_means.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas
+from math import log10
 from skimage import img_as_float
 from sklearn.cluster import KMeans
 from skimage.io import imread, imsave
@@ -10,9 +11,7 @@ norm_image = img_as_float(image)
 height, width, deep = norm_image.shape
 
 pixels = pandas.DataFrame(np.asarray(norm_image).reshape(height * width, deep), columns=['R', 'G', 'B'])
-
-k_means = KMeans(init='k-means++', random_state=241)
-pixels['cluster'] = result = k_means.fit_predict(pixels)
+pixels['cluster'] = 0
 
 
 def apply_clustering(cluster_values):
@@ -21,10 +20,32 @@ def apply_clustering(cluster_values):
     return new_image
 
 
-means = pixels.groupby('cluster').mean().values
-mean_image = apply_clustering(means)
-imsave('images/parrot_mean.png', mean_image)
+def get_psnr(image1, image2):
+    mse = np.mean((image1 - image2) ** 2)
+    return 10 * log10(1.0 / mse)
 
-medians = pixels.groupby('cluster').median().values
-median_image = apply_clustering(medians)
-imsave('images/parrot_median.png', median_image)
+
+ans = -1
+for nc in range(1, 21):
+    k_means = KMeans(init='k-means++', random_state=241, n_clusters=nc)
+    pixels['cluster'] = k_means.fit_predict(pixels.drop(['cluster'], axis=1))
+
+    means = pixels.groupby('cluster').mean().values
+    mean_image = apply_clustering(means)
+    imsave('images/parrots_mean_' + str(nc) + '.png', mean_image)
+
+    medians = pixels.groupby('cluster').median().values
+    median_image = apply_clustering(medians)
+    imsave('images/parrots_median_' + str(nc) + '.png', median_image)
+
+    psnr_mean = get_psnr(norm_image, mean_image)
+    psnr_median = get_psnr(norm_image, median_image)
+
+    max_psnr = max(psnr_mean, psnr_median)
+
+    print('mean   ' if psnr_mean > psnr_median else 'median ', nc, max_psnr)
+
+    if max_psnr > 20 and ans == -1:
+        ans = nc
+
+print('ans =', ans)
