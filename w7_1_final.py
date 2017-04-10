98655edd8c2b2df32d2aa3677f251a772b43cd46
Dmitry <scannorone@gmail.com>
2017-04-10 14:18:14 +0700

    Логистическая регрессия

--- w7_1_final.py
+++ w7_1_final.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
 data = pandas.read_csv('samples/features.csv', index_col='match_id')
@@ -14,7 +15,7 @@ data_size = len(data)
 pass_result = reversed(sorted(map(lambda nc: (1 - nc[0] / data_size, nc[1]),
                                   filter(lambda nc: nc[0] < data_size,
                                          [(data[col_name].count(), col_name) for col_name in data.columns]))))
-print('\n'.join(map(lambda nc: nc[1] + ': ' + str(round(nc[0], 3)), pass_result)))
+# print('\n'.join(map(lambda nc: nc[1] + ': ' + str(round(nc[0], 3)), pass_result)))
 
 # Удаление признаков, связанных с итогами матча. Разделение данных на признаки и целевую переменную
 X = data.drop(
@@ -28,33 +29,64 @@ X = X.fillna(0)
 cv = KFold(n_splits=5, shuffle=True, random_state=241)
 
 
-# Подход 1: градиентный бустинг "в лоб"
-def start_gradient_boosting():
-    est_nums = [10, 20, 30, 50, 100, 250]
-    means = []
-    for estimators_number in est_nums:
-        print('\nestimators_number =', estimators_number)
-        clf = GradientBoostingClassifier(n_estimators=estimators_number, random_state=241)
-        start_time = datetime.datetime.now()
-        score = cross_val_score(clf, X, y, cv=cv, scoring='roc_auc', n_jobs=-1)
-        mean = np.mean(score)
-        means.append(mean)
-        print('score =', score)
-        print('mean =', mean)
-        print('time =', datetime.datetime.now() - start_time)
-
-    plt.plot(est_nums, means)
-    plt.xlabel('estimators_number')
-    plt.ylabel('mean')
+def plot_chart(x, y, x_label, y_label):
+    plt.plot(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.show()
 
-# start_gradient_boosting()
+
+# Подход 1: градиентный бустинг "в лоб"
+def start_gradient_boosting(x_in):
+    est_nums = [10, 20, 30, 50, 100, 250]
+    means = []
+
+    for estimators_number in est_nums:
+        print('\nestimators_number =', estimators_number)
+
+        clf = GradientBoostingClassifier(n_estimators=estimators_number, random_state=241)
+
+        start_time = datetime.datetime.now()
+        score = cross_val_score(clf, x_in, y, cv=cv, scoring='roc_auc', n_jobs=-1)
+        finish_time = datetime.datetime.now() - start_time
+
+        mean = np.mean(score)
+        means.append(mean)
+
+        print('score =', score)
+        print('mean =', mean)
+        print('time =', finish_time)
+
+    return est_nums, means
+
+# nums, scores = start_gradient_boosting(X)
+# plot_chart(nums, scores, 'estimators_number', 'mean')
 
 
 # Подход 2: логистическая регрессия
-def start_logistic_regression():
-    scaler = StandardScaler()
-    scale_x = scaler.fit_transform(X)
-    print(scale_x)
+def start_logistic_regression(x_in):
+    powers = np.power(10.0, np.arange(-5, 6))
+    means = []
 
-start_logistic_regression()
+    for c in powers:
+        print('\nC =', c)
+
+        clf = LogisticRegression(C=c, random_state=241, n_jobs=1)
+
+        start_time = datetime.datetime.now()
+        score = cross_val_score(clf, x_in, y, cv=cv, scoring='roc_auc', n_jobs=1)
+        finish_time = datetime.datetime.now() - start_time
+
+        mean = np.mean(score)
+        means.append(mean)
+
+        print('score =', score)
+        print('mean =', mean)
+        print('time =', finish_time)
+
+    return powers, means
+
+scaler = StandardScaler()
+scale_x = scaler.fit_transform(X)
+c_parameters, scores = start_logistic_regression(scale_x)
+plot_chart(np.log10(c_parameters), scores, 'log10(C)', 'mean')
